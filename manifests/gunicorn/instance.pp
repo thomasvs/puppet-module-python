e14b3936d7353f75217a9089a5a90c437c23b012
Eivind Uggedal <eivind@uggedal.com>
2011-08-07 15:57:39 +0200

    Ensure src directory is created with the right permissions for gunicorn instance.

--- manifests/gunicorn/instance.pp
+++ manifests/gunicorn/instance.pp
@@ -36,6 +36,12 @@ define python::gunicorn::instance($venv,
   }
 
   if $is_present {
+    file { $src:
+      ensure => directory,
+      owner => $owner,
+      group => $group,
+    }
+
     python::pip::install {
       "$gunicorn_package in $venv":
         package => $gunicorn_package,
