76740b09f11ffb4b5b1e39e11af0d2d8d72ac5f2
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2013-01-21 16:58:01 +0100

    Fix regexp for operatingsystem == Fedora

--- manifests/dev.pp
+++ manifests/dev.pp
@@ -7,8 +7,8 @@ class python::dev($ensure=present, $version=latest) {
 
   # python development packages depends on the correct python package:
   $package_suffix = $operatingsystem ? {
-    /(?i)centos|fedora|redhat/ => 'devel',
-    default                    => 'dev',
+    /(?i:centos|fedora|redhat)/ => 'devel',
+    default                     => 'dev',
   }
 
   package { "${python}-${package_suffix}":
